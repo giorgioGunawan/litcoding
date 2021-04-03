198c4b42362ad49cf871d3c27f2bfb1e500a17dc
Giorgio Gunawan <50162021+giorgioGunawan@users.noreply.github.com>
2021-04-03 19:15:58 +1100

    Update BestTimeToBuyStock-e.java

--- BestTimeToBuyStock-e.java
+++ BestTimeToBuyStock-e.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = 10^4;
+        int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
         for(int i = 0; i < prices.length; i++){
             if(prices[i] < minPrice){
                 minPrice = prices[i];
-            }else if(prices[i] - minPrice > maxProfit){
+            }else if((prices[i] - minPrice) > maxProfit){
                 maxProfit = prices[i] - minPrice;
             }
         }
