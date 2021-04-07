e57b25af4d12100410359ce26e9265453add6238
Giorgio Gunawan <50162021+giorgioGunawan@users.noreply.github.com>
2021-04-07 20:29:19 +1000

    Create Same-Tree-e.java

---
+++ Same-Tree-e.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return recur(p, q);
+    }
+    
+    private boolean recur(TreeNode p, TreeNode q){
+        
+        if(p==null && q==null)
+            return true;
+        if((p == null && q != null) || (p != null && q == null))
+            return false;
+        if(p.val != q.val)
+            return false;
+        return recur(p.left,q.left) && recur(p.right, q.right);
+
+    }
+}
